0dceffe5561ff7a03263492d8ab3b8cc4f4e373b
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-01-11 14:41:13 +0900

    파일 생성

---
+++ 00.Algorithms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00.Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
