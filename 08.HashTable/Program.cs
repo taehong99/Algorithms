3844c13732921a6fb885d18db8e1fcd1a387ed4c
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-01-18 15:28:58 +0900

    feat : cheatKey

---
+++ 08.HashTable/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _08.HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheatKey cheats = new CheatKey();
+            cheats.Run("show me the money");
+            cheats.Run("there is no cow level");
+            cheats.Run("there is no sheep level");
+            cheats.Run("black sheep wall");
+        }
+    }
+}
