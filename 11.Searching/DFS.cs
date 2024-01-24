b58c1d54a5d5ac82613d4e7f05bbd42979a4936d
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-01-24 14:45:48 +0900

    feat : 탐색 과제

---
+++ 11.Searching/DFS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Searching
+{
+    internal class DFS
+    {
+        public static void FindPath(in bool[,] graph, out int[] path)
+        {
+            List<int> list = new List<int>();
+            dfs(0, graph, list, new bool[graph.GetLength(0)]);
+            path = list.ToArray();
+        }
+
+        private static void dfs(int cur, bool[,] graph, List<int> path, bool[] visited)
+        {
+            if (visited[cur])
+            {
+                return;
+            }
+            path.Add(cur);
+            visited[cur] = true;
+
+            for(int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[cur, i] == true)
+                {
+                    dfs(i, graph, path, visited);
+                }
+            }
+        }
+    }
+}
